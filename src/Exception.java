b6e8595ac68ea9c4b2e3ba510fdadb6d7485ac62
RamadhanFakih_ <116941345+soutPrivateClass@users.noreply.github.com>
2022-11-09 22:33:41 +0700

    Update Exception.java

--- src/Exception.java
+++ src/Exception.java
@@ -4,6 +4,14 @@ public class Exception {
     // Exception adalah penangkapan error yang terjadi saat runtime
     // untuk menangkap error agar supaya program tetap berjalan
     // menggunakan keywords try, catch, finally
+    
+    // Beberapa Exception yang sering terjadi pada java diantaranya adalah :
+    // 1. Arithmetic Exception : untuk perhitungan aritmatika, misalkan sebuah angka dibagi 0
+    // 2. Null Pointer Exception : bila object yang ingin diakses masih null, maka tidak bisa akses method dari object tersebut
+    // 3. Number Format Exception : bila ingin melakukan casting tipe data String ke int maka akan terjadi kesalahan ini
+    // 4. IO Exception : bila yang diminta adalah angka, tapi yang diinputkan adalah String
+    // 5. ArrayOutOfBoundsException : bila index yang diinputkan melebihi kapasitas yang sebelumnya sudah didefinisikan
+   
 
 
     public static void main(String[] args) {
@@ -15,12 +23,11 @@ public class Exception {
         index = in.nextInt();
         try {
             System.out.printf("index %d, adalah %d\n", index, array[index]);
-        } catch (Throwable ex){
-            System.err.println(ex);
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.err.println(e);
         } finally {
-        }
             System.out.println("program akhir");
-        System.out.println("Sukses!");
-
+            System.out.println("Sukses!");
+        }
     }
 }
